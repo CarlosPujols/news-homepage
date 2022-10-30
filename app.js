71c4fee7268b4c039f6d9c996b27c1fa699ee593
Carlos-Pujols <carlospujols94@gmail.com>
2022-10-30 08:59:28 -0400

    Added background dimmer

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var menuButton = document.getElementById('menu-button');
 var navBar = document.getElementById('navbar');
+var backgroundDimmer = document.getElementById('background-dimmer');
 
 menuButton.addEventListener("click", showMenu);
 
 function  showMenu(){
     console.log('Clicked menu button');
     navBar.classList.toggle('active');
+    backgroundDimmer.classList.toggle('active');
     menuButton.classList.toggle('close-menu');
 }
